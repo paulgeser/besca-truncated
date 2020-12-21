57857a23abbce006afc7ae65ca4780b5af3d83d0
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-12-21 13:20:38 +0100

    Adding back report function

--- besca/tl/auto_annot/_auto_annot.py
+++ besca/tl/auto_annot/_auto_annot.py
@@ -1,30 +1,26 @@
-<<<<<<< HEAD
 import csv
 import os
-=======
->>>>>>> 9d06da3 (Adding back report functions (from commit f714872e6e53e3f749d69097e097c029c0adf571))
 import sys
-import os
-import csv
-import scipy
-import scanpy as sc
-import scvelo as scv
-import pandas as pd
-import numpy as np
+
 import matplotlib
 import matplotlib.pyplot as plt
-from sklearn.svm import LinearSVC,SVC
-from sklearn.linear_model import SGDClassifier, LogisticRegressionCV, LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score, make_scorer, accuracy_score, classification_report, confusion_matrix
-from sklearn.utils.multiclass import unique_labels
+import numpy as np
+import pandas as pd
+import scanorama as scan
+import scanpy as sc
+import scipy
+import scvelo as scv
 from sklearn.calibration import CalibratedClassifierCV
-from sklearn.model_selection import cross_validate
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import (LogisticRegression, LogisticRegressionCV,
+                                  SGDClassifier)
+from sklearn.metrics import (accuracy_score, classification_report,
+                             confusion_matrix, f1_score, make_scorer)
+from sklearn.model_selection import (GridSearchCV, StratifiedShuffleSplit,
+                                     cross_validate, train_test_split)
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split, StratifiedShuffleSplit, GridSearchCV
-import scanorama as scan 
-
-from .._annot_compare import report as report_generic
+from sklearn.svm import SVC, LinearSVC
+from sklearn.utils.multiclass import unique_labels
 
 
 def read_data(train_paths, train_datasets, test_path, test_dataset, use_raw = False):
@@ -912,58 +908,3 @@ def predict_proba(classifier, scaler, adata_pred, threshold = 0):
     pred.to_csv("SVM_Pred_Labels_inter_jupyter.csv", index=False)
 
     return results, prob 
-
-
-
-
-
-def report(adata_pred, celltype, method, analysis_name, train_datasets, test_dataset, merge, use_raw = False, genes_to_use = 'all', remove_nonshared = False, clustering = 'leiden', asymmetric_matrix = True):
-    """ reports basic metrics, produces confusion matrices and plots umap of prediction
-    Writes out a csv file containing all accuracy and f1 scores.
-    Writes normalized and absolute confusion matrices, as well as umap prediction comparisons to ./figures.
-    
-    parameters
-    ----------
-    adata_pred: AnnData
-        original adata object with 'auto_annot' column
-    celltype: `str`
-        celltype column on which the prediction was performed
-    method: `str`
-        method that was used for prediction.
-    analysis_name: `str`
-        name of the analyis, used for writing files
-    train_datasets: `list`
-        list of used training datasets
-    test_dataset: `str`
-        name of test dataset
-    merge: `str`
-        what merging was performed
-    use_raw: `bool`  | default = False
-        if anndata.raw was used
-    genes_to_use: `list` or `string` | default = 'all'
-        what geneset wsa used
-    remove_nonshared: `bool`|default = False
-    clustering: `str` | default = leiden
-        clustering that was used in original analysis of testing set, needed for umap plotting
-    asymmetric_matrix: `bool` | default = True
-        if False returns square confusion matrix, if True it only shows possible combinations
-    returns
-    -------
-    0
-    """
-    fig = report_generic(
-        adata_pred = adata_pred,
-        celltype = celltype,
-        method = method,
-        analysis_name = analysis_name,
-        train_datasets = train_datasets,
-        test_dataset = test_dataset,
-        merge = merge,
-        name_prediction="auto_annot",
-        name_report="auto_annot",
-        use_raw=use_raw,
-        genes_to_use=genes_to_use,
-        remove_nonshared=remove_nonshared,
-        clustering=clustering,
-        asymmetric_matrix=asymmetric_matrix,
-)
