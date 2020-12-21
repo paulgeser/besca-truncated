0313ae419af90194334e5caf61629f149d6f0391
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-12-21 13:19:50 +0100

    Adding back report functions (from commit f714872e6e53e3f749d69097e097c029c0adf571)

--- besca/tl/auto_annot/__init__.py
+++ besca/tl/auto_annot/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from ._auto_annot import read_data, read_raw, read_adata, merge_data, naive_merge, \
         scanorama_merge, remove_genes, intersect_genes, remove_nonshared, fit, linear_svm, \
         rbf_svm, sgd_svm, random_forest, logistic_regression, logistic_regression_ovr, logistic_regression_elastic,\
@@ -6,3 +7,8 @@ from ._auto_annot import read_data, read_raw, read_adata, merge_data, naive_merg
 __all__ = ['read_data', 'read_raw', 'read_adata', 'merge_data', 'naive_merge', 'scanorama_merge', 'remove_genes', 'intersect_genes', 'remove_nonshared',
            'fit', 'linear_svm', 'rbf_svm', 'sgd_svm', 'random_forest', 'logistic_regression','logistic_regression_ovr',
             'logistic_regression_elastic', 'adata_predict', 'predict',  'adata_pred_prob', 'predict_proba' ,'report']
+=======
+from ._auto_annot import read_data, read_raw, read_adata, merge_data, naive_merge, scanorama_merge, remove_genes, intersect_genes, remove_nonshared, fit, linear_svm, rbf_svm, sgd_svm, random_forest, logistic_regression, logistic_regression_ovr, logistic_regression_elastic, adata_predict, predict, report,  plot_confusion_matrix, adata_pred_prob, predict_proba
+
+__all__ = ['read_data', 'read_raw', 'read_adata', 'merge_data', 'naive_merge', 'scanorama_merge', 'remove_genes', 'intersect_genes', 'remove_nonshared', 'fit', 'linear_svm', 'rbf_svm', 'sgd_svm', 'random_forest', 'logistic_regression','logistic_regression_ovr', 'logistic_regression_elastic', 'adata_predict', 'predict', 'report', 'plot_confusion_matrix', 'adata_pred_prob', 'predict_proba']
+>>>>>>> 9d06da3 (Adding back report functions (from commit f714872e6e53e3f749d69097e097c029c0adf571))
